b010044ca4f5d0a06e485d9107e435dc6ed6786a
影子 <>
2020-04-27 10:10:22 +0800

    添加文件名前缀工程

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 远程安装VIPER XCode模版文件
 
 克隆项目到本地。再进入项目目录，执行install.sh脚本。最后重启Xcode.
+
+1、Module Prefix_VIPER  .swift文件带前缀
+2、Module VIPER .swift文件不带前缀
---
+++ VIPER/Module Prefix_VIPER.xctemplate/Swift/___FILEBASENAME___/___VARIABLE_PrefixName______FILEBASENAME___Protocols.swift
@@ -0,0 +1,30 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+// MARK: - ModuleProtocol
+
+/// OuterSide -> ___VARIABLE_productName___
+protocol ___VARIABLE_productName___ModuleInput: class {}
+
+/// ___VARIABLE_productName___ -> OuterSide
+protocol ___VARIABLE_productName___ModuleOutput: class {}
+
+// MARK: - SceneProtocol
+
+/// Presenter -> View
+protocol ___VARIABLE_productName___ViewInput: class {}
+
+/// View -> Presenter
+protocol ___VARIABLE_productName___ViewOutput {}
+
+/// Presenter -> Interactor
+protocol ___VARIABLE_productName___InteractorInput {}
+
+/// Interactor -> Presenter
+protocol ___VARIABLE_productName___InteractorOutput: class {}
+
